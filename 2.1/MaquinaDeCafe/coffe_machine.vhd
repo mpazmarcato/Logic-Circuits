20ff78520b6138c46240ee984ebfb6fd0e6e8985
Paz <63509564+mpazmarcato@users.noreply.github.com.>
2025-01-16 12:22:06 -0300

    fix coffe machine

--- 2.1/MaquinaDeCafe/coffe_machine.vhd
+++ 2.1/MaquinaDeCafe/coffe_machine.vhd
@@ -5,7 +5,7 @@ use ieee.numeric_std.all;
 entity coffe_machine is
 	port (
 		buttons    : in std_logic_vector(7 downto 0);
-		coffe_out  : out std_logic_vector(8 downto 0);
+		coffe_out  : out std_logic_vector(7 downto 0);
 		error_flag : out std_logic
 	);
 end entity coffe_machine;
@@ -16,6 +16,7 @@ begin
 	process(buttons)
 		variable count : integer := 0;
 	begin
+		count := 0;
 		for i in buttons'range loop
 			if buttons(i) = '1' then
 				count := count + 1;
---
+++ 2.1/MaquinaDeCafe/output_files/CoffeMachine.cdf
@@ -0,0 +1,13 @@
+/* Quartus Prime Version 23.1std.0 Build 991 11/28/2023 SC Lite Edition */
+JedecChain;
+	FileRevision(JESD32A);
+	DefaultMfr(6E);
+
+	P ActionCode(Cfg)
+		Device PartName(EP4CE115F29) Path("/home/paz/4Periodo/github/Logic-Circuits/2.1/MaquinaDeCafe/output_files/") File("CoffeMachine.sof") MfrSpec(OpMask(1));
+
+ChainEnd;
+
+AlteraBegin;
+	ChainType(JTAG);
+AlteraEnd;
