6f335cc0f1f60b1c745e44f3bf7000c289a636fd
Vinicius de Lima <vinicius.lima.115@ufrn.edu.br>
2025-01-30 12:07:11 -0300

    feat: 3.1 frontend

--- 3.1/altera_frontend.vhd
+++ 3.1/altera_frontend.vhd
@@ -5,75 +5,110 @@ use IEEE.numeric_std.all;
 entity altera_frontend is
     Port (
         clk      : in  STD_LOGIC;
-        reset    : in  STD_LOGIC;
         SW       : in  STD_LOGIC_VECTOR(15 downto 0);
         KEY      : in  STD_LOGIC_VECTOR(3 downto 0);
         LEDR     : out STD_LOGIC_VECTOR(15 downto 0);
-        LEDG     : out STD_LOGIC;
-        HEX      : out STD_LOGIC_VECTOR(6 downto 0)
+        HEX0, 
+        HEX1, 
+        HEX2, 
+        HEX3, 
+        HEX4 : out STD_LOGIC_VECTOR(6 downto 0)
     );
 end entity altera_frontend;
 
 architecture Behavioral of altera_frontend is
-    signal write_en  : STD_LOGIC;
-    signal read_en   : STD_LOGIC;
-    signal addr      : STD_LOGIC_VECTOR(2 downto 0);
-    signal data_in   : STD_LOGIC_VECTOR(15 downto 0);
-    signal data_out  : STD_LOGIC_VECTOR(15 downto 0);
-    signal state_out : STD_LOGIC_VECTOR(1 downto 0);
+    type state_type is (IDLE, READ_ID, WRITE_VALUE, VIEW_VALUE, RESET_S);
+    signal state         : state_type := IDLE;
+
+    signal 
+	  reg_id
+    , reg_data_in
+    , reg_data_out
+    , display_num             : STD_LOGIC_VECTOR(15 downto 0) := (others => '0');
+
+    signal state_out : STD_LOGIC_VECTOR(1 downto 0); -- stub
+
+    -- Banco de registradores
+    signal reset
+    , write_en
+    , read_en
+    -- User interface
+    , read_reg_id_bt
+    , write_reg_val_bt
+    , view_reg_val_bt
+    , reset_reg_val_bt
+    , read_reg_id_bt_last
+    , write_reg_val_bt_last
+    , view_reg_val_bt_last 
+    , reset_reg_val_bt_last : STD_LOGIC := '0';
 
 begin
-    banco_reg_inst: entity work.banco_registradores
-        port map (
-            clk      => clk,
-            reset    => reset,
-            write_en => write_en,
-            read_en  => read_en,
-            addr     => addr,
-            data_in  => data_in,
-            data_out => data_out,
-            state_out => state_out
-        );
+    
+    banco_registradores_inst: entity work.banco_registradores
+       port map(
+          clk => clk,
+          reset => reset,
+          write_en => write_en,
+          read_en => read_en,
+          addr => reg_id(2 downto 0),
+          data_in => reg_data_in,
+          data_out => reg_data_out,
+          state_out => state_out
+      );  
+
+      read_reg_id_bt <= KEY(0);
+      view_reg_val_bt <= KEY(1);
+      write_reg_val_bt <= KEY(2);
+      reset_reg_val_bt <= KEY(3);
 
     process(clk)
     begin
         if rising_edge(clk) then
-            if reset = '1' then
+            if read_reg_id_bt = '0' and read_reg_id_bt_last = '1' then
+                reset    <= '0';
                 write_en <= '0';
                 read_en  <= '0';
-                addr     <= (others => '0');
-                data_in  <= (others => '0');
-            else
-                case state_out is
-                    when "00" => -- IDLE
-                        HEX <= "1111001";
-                        write_en <= '0';
-                        read_en  <= '0';
-                        if KEY(0) = '1' then
-                            addr <= SW(2 downto 0);
-                            data_in <= SW;
-                            write_en <= '1';
-                        elsif KEY(1) = '1' then
-                            addr <= SW(2 downto 0);
-                            read_en <= '1';
-                        end if;
-                    
-                    when "01" => -- READ
-                        HEX <= "0000011";
-                        LEDR <= data_out;
-                        read_en <= '0';
-                    
-                    when "10" => -- WRITE
-                        HEX <= "0001000";
-                        write_en <= '0';
-                    
-                    when others =>
-                        HEX <= "1111111";
-                end case;
+                reg_id   <= SW;
+                state    <= READ_ID;
+            elsif write_reg_val_bt = '0' and write_reg_val_bt_last = '1' then
+                reset       <= '0';
+                write_en    <= '1';
+                read_en     <= '0';
+                reg_data_in <= SW;
+                state       <= WRITE_VALUE;
+            elsif view_reg_val_bt = '0' and view_reg_val_bt_last = '1' then
+                reset       <= '0';
+                write_en    <= '0';
+                read_en     <= '1';
+                state       <= VIEW_VALUE;
+            elsif reset_reg_val_bt = '0' and reset_reg_val_bt_last = '1' then
+                reset    <= '1';
+                write_en <= '0';
+                read_en  <= '0';
+                state    <= RESET_S;
             end if;
+            read_reg_id_bt_last    <= read_reg_id_bt;
+            write_reg_val_bt_last  <= write_reg_val_bt;
+            view_reg_val_bt_last   <= view_reg_val_bt; 
+            reset_reg_val_bt_last  <= reset_reg_val_bt;
         end if;
     end process;
 
-    LEDG <= '1' when state_out /= "00" else '0';
+    with state select
+        display_num <= (others => '0') when IDLE,
+                       reg_id          when READ_ID,
+                       reg_data_in     when WRITE_VALUE,
+                       reg_data_out    when VIEW_VALUE,
+                       (others => '1') when RESET_S;
+
+    with state  select
+        HEX4 <= "0000000" when IDLE,
+                "1111001" when READ_ID,     -- I
+                "0000110" when WRITE_VALUE, -- E
+                "0010010" when VIEW_VALUE,  -- 0
+                "0000111" when RESET_S;
+
+    hex_to_7seg_inst: entity work.hex_to_7seg port map (display_num, HEX0, HEX1, HEX2, HEX3);
+    LEDR <= display_num;
 
 end architecture Behavioral;
